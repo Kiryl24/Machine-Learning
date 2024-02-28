bdd03d526d97340d1eebbeb1e4f240e21cbb2d5b
Pigulak <148371832+Pigulak@users.noreply.github.com>
2024-02-28 20:24:03 +0100

    + kilka punktów

--- PythonCode/MachinelearningScript/Script.py
+++ PythonCode/MachinelearningScript/Script.py
@@ -1,8 +1,10 @@
 # Pomysły na biblioteki
-# 1. Numpy
+# 1. NumPy
 # 2. Pandas
 # 3. MatPlotLib
 # 4. SciKit-Learn
+# 5. opencv-python
+# 6. Słuszne bedzie zainstalowanie ffmpeg jeśli chcemy zmieniać formaty video
 # Linki do poradników yt:
 # https://www.youtube.com/watch?v=7eh4d6sabA0&t=771s
 # https://www.youtube.com/watch?v=AxIc-vGaHQ0
