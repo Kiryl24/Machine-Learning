d326a3fae6b131455efb372bba8bed537c10c956
Pigulak <148371832+Kiryl24@users.noreply.github.com>
2024-04-22 14:24:27 +0200

    koolejny krok

--- PythonCode/MachinelearningScript/Script.py
+++ PythonCode/MachinelearningScript/Script.py
@@ -5,6 +5,7 @@
 # 4. SciKit-Learn
 # 5. opencv-python
 # 6. Słuszne bedzie zainstalowanie ffmpeg jeśli chcemy zmieniać formaty video
+# 7. Keras
 # Linki do poradników yt:
 # https://www.youtube.com/watch?v=7eh4d6sabA0&t=771s
 # https://www.youtube.com/watch?v=AxIc-vGaHQ0
